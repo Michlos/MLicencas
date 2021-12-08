22336ef20d15c0f475d403cc4ca3604b326dd3ff
MichlosBSB <michlos@gmail.com>
2021-12-08 08:57:48 -0300

    Criação de Interface IPermissõesRepository

---
+++ ServicesLayer/Usuarios/IPermissoesRepository.cs
@@ -0,0 +1,14 @@
+﻿using DomainLayer.Usuarios;
+
+namespace ServicesLayer.Usuarios
+{
+    public interface IPermissoesRepository
+    {
+        IPermissaoModel Add(IPermissaoModel permissao);
+        void Enable(int permissaoId);
+        void Desable(int permissaoId);
+        bool GetPermissao(IPermissaoModel permissao);
+
+
+    }
+}
