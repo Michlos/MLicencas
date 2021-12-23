4527c258bc9d7bb89cedf7b9bc339b971c5e9e0e
MichlosBSB <michlos@gmail.com>
2021-12-23 10:02:51 -0300

    CONTRATOS - Incisos. Inclusão do form de e/adição de inciso.

---
+++ UCViews/Contratos/ContratoIncisoUC.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace MLicencas.UCViews.Contratos
+{
+    partial class ContratoIncisoUC
+
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbTermo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picbSave = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picbSave)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txbTermo
+            // 
+            this.txbTermo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txbTermo.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txbTermo.Location = new System.Drawing.Point(3, 23);
+            this.txbTermo.Multiline = true;
+            this.txbTermo.Name = "txbTermo";
+            this.txbTermo.Size = new System.Drawing.Size(437, 157);
+            this.txbTermo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Termo:";
+            // 
+            // picbSave
+            // 
+            this.picbSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picbSave.Image = global::MLicencas.Properties.Resources.iconSave20x20;
+            this.picbSave.Location = new System.Drawing.Point(446, 158);
+            this.picbSave.Name = "picbSave";
+            this.picbSave.Size = new System.Drawing.Size(22, 22);
+            this.picbSave.TabIndex = 2;
+            this.picbSave.TabStop = false;
+            this.picbSave.Click += new System.EventHandler(this.picbSave_Click);
+            // 
+            // ContratoIncisoUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.picbSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbTermo);
+            this.Name = "ContratoIncisoUC";
+            this.Size = new System.Drawing.Size(480, 200);
+            this.Load += new System.EventHandler(this.ContratoClausulaUC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picbSave)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbTermo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picbSave;
+    }
+}
---
+++ UCViews/Contratos/ContratoIncisoUC.cs
@@ -0,0 +1,113 @@
+﻿using DomainLayer.Clientes.Contratos.Clausulas;
+using DomainLayer.Clientes.Contratos.Incisos;
+
+using InfraStructure;
+using InfraStructure.Repository.Clausulas;
+using InfraStructure.Repository.Incisos;
+
+using ServiceLayer.CommonServices;
+
+using ServicesLayer.Clausulas;
+using ServicesLayer.Contratos.Incisos;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLicencas.UCViews.Contratos
+{
+    public partial class ContratoIncisoUC : UserControl
+    {
+        private int clausulaId, incisoId;
+        private QueryStringServices _queryString;
+        private IncisosServices _incisosServices;
+
+        private IIncisoModel incisoModel;
+
+        public ContratoIncisoUC()
+        {
+
+        }
+
+        public ContratoIncisoUC(int clausulaId, int incisoId)
+        {
+            LoadServices();
+            InitializeComponent();
+            this.clausulaId = clausulaId;
+            this.incisoId = incisoId;
+        }
+
+        private void LoadServices()
+        {
+            _queryString = new QueryStringServices(new QueryString());
+            _incisosServices = new IncisosServices(new IncisoRepository(_queryString.GetQueryApp()), new ModelDataAnnotationCheck());
+        }
+
+        private void picbSave_Click(object sender, EventArgs e)
+        {
+            incisoModel = new IncisoModel();
+            incisoModel.Id = incisoId;
+            incisoModel.Termo = txbTermo.Text;
+            incisoModel.ClausulaId = clausulaId;
+
+            if (incisoId != 0)
+            {
+                UpdateInciso();
+            }
+            else
+            {
+                AddInciso();
+            }
+        }
+
+
+        private void AddInciso()
+        {
+            try
+            {
+                this.incisoModel = _incisosServices.Add(incisoModel);
+                MessageBox.Show("Termo Adicionado com sucesso.");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, Application.ProductName);
+                throw new Exception();
+            }
+        }
+
+        private void UpdateInciso()
+        {
+            try
+            {
+                _incisosServices.Edit(incisoModel);
+                MessageBox.Show("Termo Atualizado com sucesso.");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, Application.ProductName);
+                throw new Exception();
+            }
+        }
+        private void ContratoClausulaUC_Load(object sender, EventArgs e)
+        {
+            LoadFormFields();
+        }
+
+        private void LoadFormFields()
+        {
+            if (incisoId != 0)
+            {
+                this.incisoModel = new IncisoModel();
+                incisoModel = _incisosServices.GetById(incisoId);
+                txbTermo.Text = this.incisoModel.Termo;
+            }
+
+        }
+    }
+}
